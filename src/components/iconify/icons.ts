8fc3a06bef08dfa7c8d94e228f53e91cc1d0f490
Roy Ayala <troysg8203@gmail.com>
2025-08-24 19:20:12 -0400

    feat: ✨ add component Iconify

---
+++ src/components/iconify/icons.ts
@@ -0,0 +1,7 @@
+export enum Icons {
+  MENU = "solar:hamburger-menu-bold-duotone",
+  SQUARE = "solar:three-squares-bold-duotone",
+  FACEBOOK = "mingcute:facebook-fill",
+}
+
+export type IconType = Icons;
