d50a8eebc935d01cedbffe7594e2a565a638769c
Roy Ayala <troysg8203@gmail.com>
2025-08-28 22:26:38 -0400

    feat: ✨ create card and stack-card components

--- src/components/iconify/icons.ts
+++ src/components/iconify/icons.ts
@@ -4,6 +4,23 @@ export enum Icons {
   FACEBOOK = "mingcute:facebook-fill",
   MENU = "mingcute:menu-fill",
   SQUARE = "solar:three-squares-bold-duotone",
+
+  GIT = "skill-icons:git",
+  DOCKER = "skill-icons:docker",
+  HTML = "skill-icons:html",
+  CSS = "skill-icons:css",
+  JAVASCRIPT = "skill-icons:javascript",
+  TYPESCRIPT = "skill-icons:typescript",
+  ASTRO_WEB = "skill-icons:astro",
+  TAILWIND = "skill-icons:tailwindcss-light",
+  FIGMA = "skill-icons:figma-light",
+  REACT = "skill-icons:react-dark",
+  NESTJS = "skill-icons:nestjs-light",
+  FAST_API = "skill-icons:fastapi",
+  SUPABASE = "skill-icons:supabase-light",
+  TERRAFORM = "skill-icons:terraform-light",
+  KUBERNETES = "skill-icons:kubernetes",
+  GITHUB_ACTIONS = "skill-icons:githubactions-light",
 }
 
 export type IconType = Icons;
